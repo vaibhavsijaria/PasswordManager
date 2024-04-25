51c754c5578749183529c971a1567bed870a9a7d
NewEkansh <ekanshk7@gmail.com>
2024-04-25 16:30:47 +0530

    changed test.c

--- src/test.c
+++ src/test.c
@@ -1,27 +1,55 @@
+#include "crypto.h"
 #include "db_connection.h"
 #include "db_create.h"
-#include "db_users.h"
-#include "types.h"
-#include <sqlite3.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+bool create_user(sqlite3* db, const char* username, const char* hash, const char* salt);
+bool create_service(sqlite3* db, int user_id, const char* username, const char* service_name,
+                    const char* hash, const char* salt);
 
 int main()
 {
-  sqlite3* db = NULL;
-  if(openDb("test.db", &db))
+  const char* dbName = "test.db";
+  sqlite3* db;
+
+  if(!openDb(dbName, &db))
   {
-    if(createTable(db))
-    {
-      if(create_user(db, "vaibhav", "hash", "salt"))
-      {
-      }
-      if(create_service(db, 1, "buddha404", "cocaine.ninja", "hash", "salt"))
-      {
-      }
-      // PasswordData* temp = get_password(db, "services", "buddha404");
-      // printf("%s\n%s", temp->hash, temp->salt);
-      delete_user(db, "vaibhav");
-    }
+    printf("Failed to open database\n");
+    return 1;
   }
+
+  createTable(db);
+
+  char username[256];
+  char password[256];
+  char serviceName[256] = "ExampleService";
+  printf("Enter username: ");
+  scanf("%255s", username);
+  printf("Enter password: ");
+  scanf("%255s", password);
+
+  CryptoUtils* crypto = CryptoUtils_init();
+  const char* salt = "123456789";
+  Hash hashedPassword = CryptoUtils_hashData(crypto->mdctx, password, salt);
+
+  char hexHash[65];
+  for(int i = 0; i < EVP_MD_size(EVP_sha256()); i++)
+    printf(&hexHash[i * 2], "%02x", (unsigned char)hashedPassword[i]);
+
+  if(!create_user(db, username, hexHash, salt))
+  {
+    printf("Failed to create user\n");
+  }
+  if(!create_service(db, 1, username, serviceName, hexHash, salt))
+  {
+    printf("Failed to create service\n");
+  }
+
+  free(hashedPassword);
+  CryptoUtils_cleanup(crypto);
+  closeDb(&db);
+
+  return 0;
 }
